4082dd97e2341877ec28e79e3fc6633223fce794
Henrique Rodrigues <henrique.c.rodrigues@hotmail.com>
2021-08-15 21:30:45 -0300

    Adjust swagger

---
+++ Wevo/Models/UserLogin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wevo.Models
+{
+    public class UserLogin
+    {
+        public string password { get; set; }
+        public string email { get; set; }
+    }
+}
